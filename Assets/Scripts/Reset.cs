fe4e9973dc04edc90bab6769274528858bb09f22
DanielKegel <49718903+DanielKegel@users.noreply.github.com>
2020-01-15 19:28:13 +0100

    Welt funktioniert (so halbwegs)
    Alle Änderungen stehen grob in "Änderungen 2.txt"

---
+++ Assets/Scripts/Reset.cs
@@ -0,0 +1,18 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Reset : MonoBehaviour
+{
+
+    void Update()
+    {
+        if (Input.GetKeyDown(KeyCode.R))
+        {
+            // Move the object to the same position as the parent:
+            transform.position = new Vector3(0, 1, 0);
+
+        }
+    }
+}
+    
